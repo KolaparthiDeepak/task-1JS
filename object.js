ed04be1457cd80fc77a2332ff8711cc9812ed03d
KolaparthiDeepak <91003382+KolaparthiDeepak@users.noreply.github.com>
2021-11-26 21:11:35 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,35 @@
+var Person=
+{
+    FirstName:"",
+    LastName:"",
+    OfficeAddress:
+    {
+       DNumber:"",
+       City:"",
+       Pincode:"",
+    },
+};
+ 
+p1=new Object(Person);
+ p1.FirstName="Deepak";
+ p1.LastName="kolaparthi";
+ p1.OfficeAddress.DNumber="1-23";
+ p1.OfficeAddress.City="Chennai";
+ p1.OfficeAddress.Pincode="534320";
+ console.log(p1);
+
+ p2=new Object(Person);
+ p2.FirstName="Raj";
+ p2.LastName="Kapoor";
+ p2.OfficeAddress.DNumber="88/2-3";
+ p2.OfficeAddress.City="Delhi";
+ p2.OfficeAddress.Pincode="800456";
+ console.log(p2);
+
+ p3=new Object(Person);
+ p3.FirstName="Mark";
+ p3.LastName="Emanuel";
+ p3.OfficeAddress.DNumber="67-8";
+ p3.OfficeAddress.City="Nagpur";
+ p3.OfficeAddress.Pincode="709531";
+ console.log(p3);

c263bb3b69e815f9647b9be666d3dce27ea51d08
KolaparthiDeepak <91003382+KolaparthiDeepak@users.noreply.github.com>
2021-11-26 21:46:41 +0530

    Update object.js

--- object.js
+++ object.js
@@ -1,35 +1,35 @@
 var Person=
 {
-    FirstName:"first",
-    LastName:"last",
+    FirstName:"",
+    LastName:"",
     OfficeAddress:
     {
-       DNumber:"123",
-       City:"abc",
-       Pincode:"12345",
+       DNumber:"",
+       City:"",
+       Pincode:"",
     },
 };
  
-p1=new Object(Person);
- p1.FirstName="Deepak";
- p1.LastName="kolaparthi";
- p1.OfficeAddress.DNumber="1-23";
- p1.OfficeAddress.City="Chennai";
- p1.OfficeAddress.Pincode="534320";
- console.log(p1);
+Person.p1=new Object();
+Person.p1.FirstName="Deepak";
+Person.p1.LastName="kolaparthi";
+Person.p1.OfficeAddress.DNumber="1-23";
+Person.p1.OfficeAddress.City="Chennai";
+Person.p1.OfficeAddress.Pincode="534320";
+console.log(Person.p1);
 
- p2=new Object(Person);
- p2.FirstName="Raj";
- p2.LastName="Kapoor";
- p2.OfficeAddress.DNumber="88/2-3";
- p2.OfficeAddress.City="Delhi";
- p2.OfficeAddress.Pincode="800456";
- console.log(p2);
+Person.p2=new Object();
+Person.p2.FirstName="Raj";
+Person.p2.LastName="Kapoor";
+Person.p2.OfficeAddress.DNumber="88/2-3";
+Person.p2.OfficeAddress.City="Delhi";
+Person.p2.OfficeAddress.Pincode="800456";
+console.log(Person.p2);
 
- p3=new Object(Person);
- p3.FirstName="Mark";
- p3.LastName="Emanuel";
- p3.OfficeAddress.DNumber="67-8";
- p3.OfficeAddress.City="Nagpur";
- p3.OfficeAddress.Pincode="709531";
- console.log(p3);
+p3=new Object();
+Person.p3.FirstName="Mark";
+Person.p3.LastName="Emanuel";
+Person.p3.OfficeAddress.DNumber="67-8";
+Person.p3.OfficeAddress.City="Nagpur";
+Person.p3.OfficeAddress.Pincode="709531";
+console.log(Person.p3);
